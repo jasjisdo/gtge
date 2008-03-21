e862b108122c8ce968b6b2112bb3bcc75a3dc1d0
jan.bracker <jan.bracker@7e9cb074-ce48-0410-a09f-2fa9ee36210d>
2008-03-21 17:40:58 +0000

    Added correct package declaration.

--- GTGE/src/com/golden/gamedev/object/background/CachedTileBackground.java
+++ GTGE/src/com/golden/gamedev/object/background/CachedTileBackground.java
@@ -14,6 +14,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+package com.golden.gamedev.object.background;
+
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.VolatileImage;
